40da37d3f18551d4a2436c875fe4efb291bcbb3b
Kamil Oblej <grubyosno@gmail.com>
2020-11-16 07:53:32 +0100

    when data is taken date is saved in database

--- BackEnd/server/camera/models.py
+++ BackEnd/server/camera/models.py
@@ -8,7 +8,7 @@ class Photo(models.Model):
 
     name = models.CharField(max_length=200)
     image = models.ImageField()
-    date_taken = models.DateTimeField(null=True)
+    date_taken = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.name
@@ -18,7 +18,7 @@ class Thermo(models.Model):
 
     name = models.CharField(max_length=200)
     image = models.ImageField()
-    date_taken = models.DateTimeField(null=True)
+    date_taken = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.name
@@ -28,7 +28,7 @@ class Temperature(models.Model):
 
     temperature1 = models.CharField(max_length=20)
     temperature2 = models.CharField(max_length=20)
-    date_taken = models.DateTimeField(null=True)
+    date_taken = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return str(self.temperature1)+ ' ' +str(self.temperature2)
